f9ccdb7e6daf44b3a99486e44135aea096e35705
jaylen curry <curryjaylen24@gmail.com>
2025-05-14 14:10:32 -0400

    new portfolio

---
+++ app/components/openBtn.tsx
@@ -0,0 +1,12 @@
+'use client';
+import React from 'react'
+
+const openBtn = () => {
+  return (
+    <div>
+      <button >Open to meet me!! </button>
+    </div>
+  )
+}
+
+export default openBtn
---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Openbtn from "./components/openBtn";
+
+export default function Home() {
+  return (
+   <>
+    <div className="h-screen flex items-center justify-center">
+      <Openbtn/>
+    </div>
+   </> 
+  );
+}
